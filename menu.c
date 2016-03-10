7f6e2e80061e4490788b7e8cc3992096fe2deddd
Shaun McFall <shaunm.mcfall@gmail.com>
2016-03-09 20:19:02 -0500

    Functional selections

--- menu.c
+++ menu.c
@@ -39,6 +39,8 @@ int DisplayMenu(Menu* menu) {
     uint8_t linePadding = 10;
     uint8_t textPadding = 10;
     uint8_t extraTextPadding = 0;
+    uint8_t previouslySelectedIndex = 0;
+    bool selected = false;
 
     if (menu->SelectionType != None) {
         extraTextPadding = linePadding;
@@ -52,12 +54,19 @@ int DisplayMenu(Menu* menu) {
 			gc_PrintStringXY(menu->Items[i].Name, textPadding + extraTextPadding, linePadding + linePadding * i);
 
             if (menu->SelectionType != None && menu->Items[i].Function != FUNCTION_BACK) {
+                if (menu->Items[i].Selected) {
+                    previouslySelectedIndex = i;
+                    selected = true;
+                } else {
+                    selected = false;
+                }
+
                 switch (menu->SelectionType) {
                     case Single:
-                        gc_NoClipDrawSprite(menu->Items[i].Selected ? radiobutton_filled : radiobutton_empty, textPadding, linePadding + linePadding * i - 1, 9, 9);
+                        gc_NoClipDrawSprite(selected ? radiobutton_filled : radiobutton_empty, textPadding, linePadding + linePadding * i - 1, 9, 9);
                         break;
                     case Multiple:
-                        gc_NoClipDrawSprite(menu->Items[i].Selected ? checkbox_checked : checkbox_empty, textPadding, linePadding + linePadding * i - 1, 9, 9);
+                        gc_NoClipDrawSprite(selected ? checkbox_checked : checkbox_empty, textPadding, linePadding + linePadding * i - 1, 9, 9);
                         break;
                 }
             }
@@ -75,8 +84,24 @@ int DisplayMenu(Menu* menu) {
 		if (Key_IsDown(Key_Up)) { y = y == 1 ? menu->NumItems : y - 1; }
 		else if (Key_IsDown(Key_Down)) { y = y == menu->NumItems ? 1 : y + 1; }
 		else if (Key_IsDown(Key_2nd) || Key_IsDown(Key_Enter)) {
-			void (*func)() = menu->Items[y - 1].Function;
-			if (func == FUNCTION_BACK) { return y; }
+            uint8_t index = y - 1;
+            void(*func)() = menu->Items[index].Function;
+
+            if (menu->SelectionType != None && menu->Items[y - 1].Function != FUNCTION_BACK) {
+                switch (menu->SelectionType) {
+                    case Single:
+                        if (index != previouslySelectedIndex) {
+                            menu->Items[previouslySelectedIndex].Selected = false;
+                            menu->Items[index].Selected = true;
+                        }
+                        break;
+                    case Multiple:
+                        menu->Items[index].Selected = !menu->Items[index].Selected;
+                        break;
+                }
+            }
+			
+			if (func == FUNCTION_BACK) { return index; }
 			else if (func != FUNCTION_NONE) { func(); }
 			gc_FillScrn(255);
 		}
